69d3e52c4668a04b1d59cfb049f2a9b1c3c59d43
keep-sharp-mind <138483185+keep-sharp-mind@users.noreply.github.com>
2024-06-05 16:28:04 +0800

    Update b_plus_tree_page.cpp

--- src/page/b_plus_tree_page.cpp
+++ src/page/b_plus_tree_page.cpp
@@ -1,15 +1,13 @@
 #include "page/b_plus_tree_page.h"
 
-// huangzheng 22.5.10 22:00 v1
-// all
-
 /*
  * Helper methods to get/set page type
  * Page type enum class is defined in b_plus_tree_page.h
  */
-bool BPlusTreePage::IsLeafPage() const 
-{
-  // return false;
+/**
+ * TODO: Student Implement
+ */
+bool BPlusTreePage::IsLeafPage() const {
   if(this->page_type_ == IndexPageType::LEAF_PAGE)
   {
     return true;
@@ -17,8 +15,10 @@ bool BPlusTreePage::IsLeafPage() const
   return false;
 }
 
-bool BPlusTreePage::IsRootPage() const 
-{
+/**
+ * TODO: Student Implement
+ */
+bool BPlusTreePage::IsRootPage() const {
   if(this->parent_page_id_ == INVALID_PAGE_ID)
   {
     return true;
@@ -26,42 +26,51 @@ bool BPlusTreePage::IsRootPage() const
   return false;
 }
 
-void BPlusTreePage::SetPageType(IndexPageType page_type) 
-{
+/**
+ * TODO: Student Implement
+ */
+void BPlusTreePage::SetPageType(IndexPageType page_type) {
   this->page_type_ = page_type;
+}
+
+int BPlusTreePage::GetKeySize() const {
+  return key_size_;
+}
+
+void BPlusTreePage::SetKeySize(int size) {
+  key_size_ = size;
 }
 
 /*
  * Helper methods to get/set size (number of key/value pairs stored in that
  * page)
  */
-int BPlusTreePage::GetSize() const 
-{
-  // return 0;
-  return this->size_;
+int BPlusTreePage::GetSize() const {
+  return size_;
 }
 
-void BPlusTreePage::SetSize(int size) 
-{
-  this->size_ = size;
+void BPlusTreePage::SetSize(int size) {
+  size_ = size;
 }
 
-void BPlusTreePage::IncreaseSize(int amount) 
-{
-  this->size_ += amount;
+void BPlusTreePage::IncreaseSize(int amount) {
+  size_ += amount;
 }
 
 /*
  * Helper methods to get/set max size (capacity) of the page
  */
-int BPlusTreePage::GetMaxSize() const 
-{
-  // return 0;
+/**
+ * TODO: Student Implement
+ */
+int BPlusTreePage::GetMaxSize() const {
   return this->max_size_;
 }
 
-void BPlusTreePage::SetMaxSize(int size) 
-{
+/**
+ * TODO: Student Implement
+ */
+void BPlusTreePage::SetMaxSize(int size) {
   this->max_size_ = size;
 }
 
@@ -69,43 +78,41 @@ void BPlusTreePage::SetMaxSize(int size)
  * Helper method to get min page size
  * Generally, min page size == max page size / 2
  */
-int BPlusTreePage::GetMinSize() const 
-{
-  // return 0;
+/**
+ * TODO: Student Implement
+ */
+int BPlusTreePage::GetMinSize() const {
   return this->max_size_ / 2;
 }
 
 /*
  * Helper methods to get/set parent page id
  */
-page_id_t BPlusTreePage::GetParentPageId() const 
-{
+/**
+ * TODO: Student Implement
+ */
+page_id_t BPlusTreePage::GetParentPageId() const {
   return this->parent_page_id_;
 }
 
-void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) 
-{
-  this->parent_page_id_ = parent_page_id;
+void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) {
+  parent_page_id_ = parent_page_id;
 }
 
 /*
  * Helper methods to get/set self page id
  */
-page_id_t BPlusTreePage::GetPageId() const 
-{
-  // return INVALID_PAGE_ID;
-  return this->page_id_;
+page_id_t BPlusTreePage::GetPageId() const {
+  return page_id_;
 }
 
-void BPlusTreePage::SetPageId(page_id_t page_id) 
-{
-  this->page_id_ = page_id;
+void BPlusTreePage::SetPageId(page_id_t page_id) {
+  page_id_ = page_id;
 }
 
 /*
  * Helper methods to set lsn
  */
-void BPlusTreePage::SetLSN(lsn_t lsn) 
-{
+void BPlusTreePage::SetLSN(lsn_t lsn) {
   lsn_ = lsn;
 }
